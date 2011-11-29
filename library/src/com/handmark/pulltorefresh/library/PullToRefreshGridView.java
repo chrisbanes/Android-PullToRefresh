8891cf84d83c067cd5ade558abb62eb3f42d393a
Chris Banes <chris@senab.co.uk>
2011-11-29 19:27:12 +0000

    Remove Unnecessary AdapterView Inner Classes

--- library/src/com/handmark/pulltorefresh/library/PullToRefreshGridView.java
+++ library/src/com/handmark/pulltorefresh/library/PullToRefreshGridView.java
@@ -3,21 +3,8 @@ package com.handmark.pulltorefresh.library;
 import android.content.Context;
 import android.util.AttributeSet;
 import android.widget.GridView;
-import android.widget.ListAdapter;
 
 public class PullToRefreshGridView extends PullToRefreshBase<GridView> {
-
-	private class PullToRefreshInternalGridView extends GridView {
-
-		PullToRefreshInternalGridView(Context context, AttributeSet attrs) {
-			super(context, attrs);
-		}
-
-		public void setAdapter(ListAdapter adapter) {
-			super.setAdapter(adapter);
-			resetHeader();
-		}
-	}
 
 	public PullToRefreshGridView(Context context) {
 		super(context);
@@ -29,7 +16,7 @@ public class PullToRefreshGridView extends PullToRefreshBase<GridView> {
 
 	@Override
 	protected GridView createAdapterView(Context context, AttributeSet attrs) {
-		GridView gv = new PullToRefreshInternalGridView(context, attrs);
+		GridView gv = new GridView(context, attrs);
 
 		// Use Generated ID (from res/values/ids.xml)
 		gv.setId(R.id.gridview);
