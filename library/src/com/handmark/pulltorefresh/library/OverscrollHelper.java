f8aa70b43b174f729f79c8de32c4e86c85238819
Chris Banes <chris@senab.co.uk>
2012-09-12 15:53:23 +0100

    Fix Overscroll for ScrollView

--- library/src/com/handmark/pulltorefresh/library/OverscrollHelper.java
+++ library/src/com/handmark/pulltorefresh/library/OverscrollHelper.java
@@ -8,10 +8,52 @@ import com.handmark.pulltorefresh.library.PullToRefreshBase.Mode;
 
 @TargetApi(9)
 final class OverscrollHelper {
-	
+
 	static final String LOG_TAG = "OverscrollHelper";
 
+	/**
+	 * Helper method for Overscrolling that encapsulates all of the necessary
+	 * function.
+	 * 
+	 * This should only be used on AdapterView's such as ListView as it just
+	 * calls through to overScrollBy() with the scrollRange = 0. AdapterView's
+	 * do not have a scroll range (i.e. getScrollY() doesn't work).
+	 * 
+	 * @param view
+	 *            - PullToRefreshView that is calling this.
+	 * @param deltaY
+	 *            - Change in Y in pixels, passed through from from overScrollBy
+	 *            call
+	 * @param scrollY
+	 *            - Current Y scroll value in pixels before applying deltaY,
+	 *            passed through from from overScrollBy call
+	 * @param isTouchEvent
+	 *            - true if this scroll operation is the result of a touch
+	 *            event, passed through from from overScrollBy call
+	 */
 	static void overScrollBy(PullToRefreshBase<?> view, int deltaY, int scrollY, boolean isTouchEvent) {
+		overScrollBy(view, deltaY, scrollY, 0, isTouchEvent);
+	}
+
+	/**
+	 * Helper method for Overscrolling that encapsulates all of the necessary
+	 * function.
+	 * 
+	 * @param view
+	 *            - PullToRefreshView that is calling this.
+	 * @param deltaY
+	 *            - Change in Y in pixels, passed through from from overScrollBy
+	 *            call
+	 * @param scrollY
+	 *            - Current Y scroll value in pixels before applying deltaY,
+	 *            passed through from from overScrollBy call
+	 * @param scrollRange
+	 *            - Scroll Range of the View, specifically needed for ScrollView
+	 * @param isTouchEvent
+	 *            - true if this scroll operation is the result of a touch
+	 *            event, passed through from from overScrollBy call
+	 */
+	static void overScrollBy(PullToRefreshBase<?> view, int deltaY, int scrollY, int scrollRange, boolean isTouchEvent) {
 
 		// Check that OverScroll is enabled
 		if (view.isPullToRefreshOverScrollEnabled()) {
@@ -20,25 +62,32 @@ final class OverscrollHelper {
 			// Check that we're not disabled, and the event isn't from touch
 			if (mode != Mode.DISABLED && !isTouchEvent) {
 				final int newY = (deltaY + scrollY);
-				
+
 				if (PullToRefreshBase.DEBUG) {
-					Log.d(LOG_TAG, "OverScroll. DeltaY: " + deltaY + " ScrollY: " + scrollY);
+					Log.d(LOG_TAG, "OverScroll. DeltaY: " + deltaY + ", ScrollY: " + scrollY + ", NewY: " + newY
+							+ ", ScrollRange: " + scrollRange);
 				}
 
-				if (newY != 0) {
+				if (newY < 0) {
 					// Check the mode supports the overscroll direction, and
 					// then move scroll
-					if ((mode.canPullDown() && newY < 0) || (mode.canPullUp() && newY > 0)) {
+					if (mode.canPullDown()) {
 						view.setHeaderScroll(view.getScrollY() + newY);
 					}
-				} else {
+				} else if (newY > scrollRange) {
+					// Check the mode supports the overscroll direction, and
+					// then move scroll
+					if (mode.canPullUp()) {
+						view.setHeaderScroll(view.getScrollY() + newY - scrollRange);
+					}
+				} else if (newY == 0 || newY == scrollRange) {
 					// Means we've stopped overscrolling, so scroll back to 0
 					view.smoothScrollTo(0, PullToRefreshBase.SMOOTH_SCROLL_LONG_DURATION_MS);
 				}
 			}
 		}
 	}
-	
+
 	static boolean isAndroidOverScrollEnabled(View view) {
 		return view.getOverScrollMode() != View.OVER_SCROLL_NEVER;
 	}
--- library/src/com/handmark/pulltorefresh/library/PullToRefreshScrollView.java
+++ library/src/com/handmark/pulltorefresh/library/PullToRefreshScrollView.java
@@ -1,6 +1,9 @@
 package com.handmark.pulltorefresh.library;
 
+import android.annotation.TargetApi;
 import android.content.Context;
+import android.os.Build.VERSION;
+import android.os.Build.VERSION_CODES;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ScrollView;
@@ -13,7 +16,13 @@ public class PullToRefreshScrollView extends PullToRefreshBase<ScrollView> {
 
 	@Override
 	protected ScrollView createRefreshableView(Context context, AttributeSet attrs) {
-		ScrollView scrollView = new ScrollView(context, attrs);
+		ScrollView scrollView;
+		if (VERSION.SDK_INT >= VERSION_CODES.GINGERBREAD) {
+			scrollView = new InternalScrollViewSDK9(context, attrs);
+		} else {
+			scrollView = new ScrollView(context, attrs);
+		}
+
 		scrollView.setId(R.id.scrollview);
 		return scrollView;
 	}
@@ -30,5 +39,39 @@ public class PullToRefreshScrollView extends PullToRefreshBase<ScrollView> {
 			return mRefreshableView.getScrollY() >= (scrollViewChild.getHeight() - getHeight());
 		}
 		return false;
+	}
+
+	@TargetApi(9)
+	final class InternalScrollViewSDK9 extends ScrollView {
+
+		public InternalScrollViewSDK9(Context context, AttributeSet attrs) {
+			super(context, attrs);
+		}
+
+		@Override
+		protected boolean overScrollBy(int deltaX, int deltaY, int scrollX, int scrollY, int scrollRangeX,
+				int scrollRangeY, int maxOverScrollX, int maxOverScrollY, boolean isTouchEvent) {
+
+			final boolean returnValue = super.overScrollBy(deltaX, deltaY, scrollX, scrollY, scrollRangeX,
+					scrollRangeY, maxOverScrollX, maxOverScrollY, isTouchEvent);
+
+			// Does all of the hard work...
+			OverscrollHelper
+					.overScrollBy(PullToRefreshScrollView.this, deltaY, scrollY, getScrollRange(), isTouchEvent);
+
+			return returnValue;
+		}
+
+		/**
+		 * Taken from the AOSP ScrollView source
+		 */
+		private int getScrollRange() {
+			int scrollRange = 0;
+			if (getChildCount() > 0) {
+				View child = getChildAt(0);
+				scrollRange = Math.max(0, child.getHeight() - (getHeight() - getPaddingBottom() - getPaddingTop()));
+			}
+			return scrollRange;
+		}
 	}
 }
