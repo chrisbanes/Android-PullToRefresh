d10ab41bb14fc343485b23e644b64dd5d2b82cde
Chris Banes <chris@senab.co.uk>
2012-12-11 16:40:54 +0000

    Fix use of deprecated methods in sample

--- sample/src/com/handmark/pulltorefresh/samples/PullToRefreshListActivity.java
+++ sample/src/com/handmark/pulltorefresh/samples/PullToRefreshListActivity.java
@@ -126,8 +126,8 @@ public final class PullToRefreshListActivity extends ListActivity {
 	public boolean onCreateOptionsMenu(Menu menu) {
 		menu.add(0, MENU_MANUAL_REFRESH, 0, "Manual Refresh");
 		menu.add(0, MENU_DISABLE_SCROLL, 1,
-				mPullRefreshListView.isDisableScrollingWhileRefreshing() ? "Enable Scrolling while Refreshing"
-						: "Disable Scrolling while Refreshing");
+				mPullRefreshListView.isScrollingWhileRefreshingEnabled() ? "Disable Scrolling while Refreshing"
+						: "Enable Scrolling while Refreshing");
 		menu.add(0, MENU_SET_MODE, 0, mPullRefreshListView.getMode() == Mode.BOTH ? "Change to MODE_PULL_DOWN"
 				: "Change to MODE_PULL_BOTH");
 		menu.add(0, MENU_DEMO, 0, "Demo");
@@ -138,8 +138,8 @@ public final class PullToRefreshListActivity extends ListActivity {
 	public boolean onPrepareOptionsMenu(Menu menu) {
 		MenuItem disableItem = menu.findItem(MENU_DISABLE_SCROLL);
 		disableItem
-				.setTitle(mPullRefreshListView.isDisableScrollingWhileRefreshing() ? "Enable Scrolling while Refreshing"
-						: "Disable Scrolling while Refreshing");
+				.setTitle(mPullRefreshListView.isScrollingWhileRefreshingEnabled() ? "Disable Scrolling while Refreshing"
+						: "Enable Scrolling while Refreshing");
 
 		MenuItem setModeItem = menu.findItem(MENU_SET_MODE);
 		setModeItem.setTitle(mPullRefreshListView.getMode() == Mode.BOTH ? "Change to MODE_FROM_START"
@@ -157,8 +157,8 @@ public final class PullToRefreshListActivity extends ListActivity {
 				mPullRefreshListView.setRefreshing(false);
 				break;
 			case MENU_DISABLE_SCROLL:
-				mPullRefreshListView.setDisableScrollingWhileRefreshing(!mPullRefreshListView
-						.isDisableScrollingWhileRefreshing());
+				mPullRefreshListView.setScrollingWhileRefreshingEnabled(!mPullRefreshListView
+						.isScrollingWhileRefreshingEnabled());
 				break;
 			case MENU_SET_MODE:
 				mPullRefreshListView.setMode(mPullRefreshListView.getMode() == Mode.BOTH ? Mode.PULL_FROM_START
