5ad18c46319cd1ca315883fa52463b5710ac363b
Moe Adham <moeadham@gmail.com>
2013-06-06 14:01:42 -0400

    Implement OnItemLongClickListener.

--- library/src/com/handmark/pulltorefresh/library/PullToRefreshAdapterViewBase.java
+++ library/src/com/handmark/pulltorefresh/library/PullToRefreshAdapterViewBase.java
@@ -28,6 +28,7 @@ import android.widget.AbsListView.OnScrollListener;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.AdapterView.OnItemLongClickListener;
 import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
@@ -206,6 +207,10 @@ public abstract class PullToRefreshAdapterViewBase<T extends AbsListView> extend
 	 */
 	public void setOnItemClickListener(OnItemClickListener listener) {
 		mRefreshableView.setOnItemClickListener(listener);
+	}
+	
+	public void setOnItemLongClickListener(OnItemLongClickListener listener) {
+		mRefreshableView.setOnItemLongClickListener(listener);
 	}
 
 	public final void setOnLastItemVisibleListener(OnLastItemVisibleListener listener) {
