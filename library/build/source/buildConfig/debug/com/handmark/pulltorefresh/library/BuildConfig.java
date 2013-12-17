7b7c63866029ba2fc71244abc1a0f926b3ef5055
Max Klimenko <max.klimenko@gmail.com>
2013-12-17 16:20:56 +0700

    Build

---
+++ library/build/source/buildConfig/debug/com/handmark/pulltorefresh/library/BuildConfig.java
@@ -0,0 +1,7 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.handmark.pulltorefresh.library;
+
+public final class BuildConfig {
+    public static final boolean DEBUG = Boolean.parseBoolean("true");
+
+}
