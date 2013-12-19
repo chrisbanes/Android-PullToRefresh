037b62c89cbc4d4df70357bbd7b6546b92f60e43
Max Klimenko <max.klimenko@gmail.com>
2013-12-19 15:19:40 +0700

    Upgrade gradle

--- library/build/source/buildConfig/debug/com/handmark/pulltorefresh/library/BuildConfig.java
+++ library/build/source/buildConfig/debug/com/handmark/pulltorefresh/library/BuildConfig.java
@@ -1,7 +1,12 @@
-/** Automatically generated file. DO NOT MODIFY */
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
 package com.handmark.pulltorefresh.library;
 
 public final class BuildConfig {
-    public static final boolean DEBUG = Boolean.parseBoolean("true");
-
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String PACKAGE_NAME = "com.handmark.pulltorefresh.library";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 2110;
 }
